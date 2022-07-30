3afb2bb1aad0450dca50ebdcbcd761a7a9f56b2e
totalnoMartina <totalnobzvz@yahoo.com>
2022-07-30 16:01:25 +0000

    Add shopping cart, checkout template

--- macrameshop/views.py
+++ macrameshop/views.py
@@ -3,4 +3,18 @@ from django.shortcuts import render
 
 def index(request):
     """ A view to display homepage """
-    return render(request, 'macrameshop/index.html')
+    context = {}
+    return render(request, 'macrameshop/index.html', context)
+
+
+def shopping_cart(request):
+    """ A view to display shopping cart """
+    context = {}
+    return render(request, 'macrameshop/shopping_cart.html', context)
+
+
+def checkout(request):
+    """ A view to display shopping cart """
+    context = {}
+
+    return render(request, 'macrameshop/checkout.html', context)
