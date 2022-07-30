b05364af8c69332a5bd4c8c055aeac0dd1f04c93
totalnoMartina <totalnobzvz@yahoo.com>
2022-07-30 23:29:36 +0000

    Add models, makemigrations, add products, arrange links in base.html

--- macrameshop/admin.py
+++ macrameshop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Buyer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(ShippingDetails)
+admin.site.register(OrderedItems)
--- macrameshop/models.py
+++ macrameshop/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Buyer(models.Model):
+    """ A class for a buyer profile """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=150, null=True)
+    email = models.EmailField(max_length=150, null=True)
+
+    def __str__(self):
+        """ A helper method to see the name of the buyer """
+        return self.name
+
+
+class Product(models.Model):
+    """ A class for a product model """
+    product = models.CharField(max_length=150, null=True)
+    description = models.TextField(max_length=900)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    date_added = models.DateField(auto_now=True)
+    # image
+
+    def __str__(self):
+        """ A helper method to see the name of the product """
+        return self.product
+
+
+class Order(models.Model):
+    """ A model for an order """
+    buyer = models.ForeignKey(Buyer, on_delete=models.SET_NULL, null=True)
+    ordered = models.DateTimeField(auto_now_add=True)
+    completed_order = models.BooleanField(default=False, blank=False)
+    transaction_id = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        """ A helper method to view transation id """
+        return str(self.id)
+
+class OrderedItems(models.Model):
+    """ A model of ordered items """
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True)
+    added = models.DateTimeField(auto_now_add=True)
+
+
+class ShippingDetails(models.Model):
+    """ A model for shipping details """
+    buyer = models.ForeignKey(Buyer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    street_address1 = models.CharField(max_length=150, null=True)
+    street_address2 = models.CharField(max_length=150, null=True)
+    city = models.CharField(max_length=150, null=True)
+    state_or_country = models.CharField(max_length=150, null=True)
+    zip_or_eircode = models.CharField(max_length=150, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """ A helper method to see the address """
+        return self.street_address1
+
+
+
+
+
+
