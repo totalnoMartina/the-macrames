8816e4cd5ff5bb783650ab925831f6662b62ae87
totalnoMartina <totalnobzvz@yahoo.com>
2022-08-17 17:40:33 +0000

    Rendering orders too finally

--- testcrudapp/views.py
+++ testcrudapp/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Product, Ordering, Customer
 
 
 def home(request):
     """ To render items in Dashboard - homepage """
-    orders = Ordering.objects.all()
+    ordering = Ordering.objects.all()
     customers = Customer.objects.all()
 
     context = {
-        'ordering': orders,
+        'ordering': ordering,
         'customers': customers
     }
     return render(request, 'testcrudapp/dashboard.html', context)
